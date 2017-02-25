1c7e492961d67502291558acec7953f4388c84e9
Arran Hinch Sisley <Arranswim@gmail.com>
2017-02-25 23:42:58 +0000

    tabbar fade

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 function main() {
   $('.title').hide().fadeIn(1800);
-  $('.aboutme').hide().fadeIn(2000);
-  $('.rustbutton').hide().fadeIn(2200);
+  $('.tabbar').hide().fadeIn(1800);
+  $('.aboutme').hide().fadeIn(2200);
+  $('.rustbutton').hide().fadeIn(2400);
   $('.rustbutton').on('click', function() {
     $(this).toggleClass('active');
     $('.ruststats').slideToggle();
