7aba966090f577a35bcb3026d96b488ae648bd22
Arran Hinch Sisley <Arranswim@gmail.com>
2017-05-02 12:30:20 +0100

    Merge

---
+++ js/HeliClock.js
@@ -0,0 +1,21 @@
+function runDelayedWithProgress(waitIntervalSeconds, delayedAction, progressIntervalSeconds, progressAction) {
+  var startTime = new Date().getTime();
+  var iid = setInterval(function() {
+     var curTime = new Date().getTime()
+     var elapsedSeconds = (curTime - startTime)/1000;
+     if(elapsedSeconds >= waitIntervalSeconds) {
+        clearInterval(iid);
+        delayedAction();
+     }
+     else {
+       progressAction(Math.round(elapsedSeconds));
+     }
+  }, progressIntervalSeconds*1000);
+}
+
+runDelayedWithProgress(30*60, function() {
+      console.log("Heli should be arriving soon!")
+   }, 
+   60, function(elapsed){
+     console.log("No heli yet! " + elapsed);
+});
